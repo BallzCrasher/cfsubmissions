de1f7e2f99ec242be4118ed1ff7bbaca493d2f02
Shadi <shadisalman11@gmail.com>
2023-01-03 11:58:05 +0300

    using argparse instead of a function

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import urllib.request
 import json
-from sys import argv
-
-OPTIONS = ['--min', '--max']
+import argparse
 BASE_URL = 'https://codeforces.com'
 
-def parse_arguments():
-    def phelp():
-        print(f'{argv[0]} <HANDLE> [--min value] [--max value]')
-        exit(1)
+# pargin argments
+parser = argparse.ArgumentParser()
+parser.add_argument("handle", type=str, help="handle of the user")
+parser.add_argument('--min', type=int, help='min value of problem rating')
+parser.add_argument('--max', type=int, help='max value of problem rating')
+config = parser.parse_args().__dict__
 
-    if len(argv) < 2: phelp()
-    arguments = dict()
-    arguments['handle'] = argv[1]
-    for i in range(2, len(argv) - 1, 2):
-        if argv[i] not in OPTIONS: phelp()
-        arguments[argv[i]] = int(argv[i + 1])
-    return arguments
-
-config = parse_arguments()
+#sending API request
 response = urllib.request.urlopen(f"{BASE_URL}/api/user.status?handle={config['handle']}")
 print("Finished requesting")
 response = json.loads(response.read().decode('utf-8'))
@@ -26,15 +18,16 @@ if response['status'] != 'OK':
     print("API request faild. Aborting...")
     exit(1)
 
+#parsing response
 for submission in response['result']:
     if 'rating' not in submission['problem']:
         continue
-    if '--min' in config and submission['problem']['rating'] < config['--min']:
+    if config['--min'] and submission['problem']['rating'] < config['--min']:
         continue
-    if '--max' in config and submission['problem']['rating'] > config['--max']:
+    if config['--max'] and submission['problem']['rating'] > config['--max']:
         continue
     contest = submission['problem']['contestId']
     sub_id = submission['id']
     print((submission['id'], submission['verdict'], submission['problem'],
-        f"{BASE_URL}/contest/{contest}/{sub_id}"))
+        f"{BASE_URL}/contest/{contest}/submission/{sub_id}"))
     
